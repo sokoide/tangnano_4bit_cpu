6a05eb5ad882d0e518cab18548471a2e6e8471f5
So Koide <sokoide@gmail.cojm>
2025-02-02 18:50:04 +0900

    initial commit

---
+++ src/ram.sv
@@ -0,0 +1,29 @@
+module ram(
+  input  logic        clk,
+  input  logic        we,
+  input  logic [7:0]  r_addr,
+  output logic [7:0]  r_data,
+  input  logic [7:0]  w_addr,
+  input  logic [7:0]  w_data
+);
+
+  // 8bit x 16 words memory space
+  logic [7:0] mem [15:0];
+
+  initial begin
+    mem[0] <= 8'b01100_110;// inc R6		
+    mem[1] <= 8'b1001_0000;// jmp 0
+  end
+
+  // write (sync clock)
+  always_ff @(posedge clk) begin
+    if (we) begin
+      mem[w_addr] <= w_data;
+    end
+  end
+
+  // read
+  assign r_data = mem[r_addr];
+
+endmodule
+
---
+++ src/top.sv
@@ -0,0 +1,48 @@
+module top(
+  input  logic       clk,
+  input  logic       rst,
+  output logic [3:0] leds
+);
+
+  // internal signals
+  logic [7:0]  w_data;
+  logic [7:0]  r_data;
+  logic [3:0]  adr;
+  logic [3:0]  led;
+  logic [3:0]  btn;
+  logic [23:0] counter;
+
+  assign btn = 4'b0000;
+
+  // output port leds are negatives of internal signal led
+  assign leds = ~led;
+
+  // RAM instance
+  ram ram1 (
+    .clk       (counter[23]),
+    .we        (1'b0),
+    .r_addr    ({4'b0000, adr}),
+    .r_data    (r_data),
+    .w_addr    ({4'b0000, adr}),
+    .w_data    (w_data)
+  );
+
+  // CPU instance
+  cpu cpu1 (
+    .reset (rst),
+    .clk   (counter[23]),
+    .btn   (btn),
+    .led   (led),
+    .adr   (adr),
+    .dout  (r_data)
+  );
+
+  // update counter
+  always_ff @(posedge clk or negedge rst) begin
+    if (!rst)
+      counter <= 24'd0;
+    else
+      counter <= counter + 1;
+  end
+
+endmodule
